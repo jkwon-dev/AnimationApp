ac7cb80c8ad425f07586674c5274e340260a8b60
jkwon-dev <kwonejk@gmail.com>
2023-12-05 15:03:24 +0900

    Initial Commit

---
+++ AnimationApp/AnimationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationAppApp.swift
+//  AnimationApp
+//
+//  Created by kwon eunji on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
